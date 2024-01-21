4eb9ec29e4daec7275ab8ded1dc1a6112f24b184
Sambit Kumar <sambit.kumar142@gmail.com>
2024-01-21 19:40:45 +0530

    added logout functionality

--- varta/accounts/urls.py
+++ varta/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import LogoutAPIView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', obtain_auth_token),
+    path('logout/', LogoutAPIView.as_view()),
 ]
--- varta/accounts/views.py
+++ varta/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+class LogoutAPIView(GenericAPIView):
+    queryset = Token.objects.all()
 
-# Create your views here.
+    def get(self, request, *args, **kwargs):
+        token = Token.objects.get(user=request.user)
+        print(token)
+        token.delete()
+        return Response(
+            {"detail": "Logout successful"},
+            status=status.HTTP_200_OK)
