a23f85ad9646bc9e75064553dc176eadafa86c1b
Sambit Kumar <sambit.kumar142@gmail.com>
2024-01-22 07:35:24 +0530

    feature: validate login

--- varta/accounts/urls.py
+++ varta/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import LogoutAPIView
+from .views import LogoutAPIView, ValidateLoggedInAPIView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('logout/', LogoutAPIView.as_view()),
+    path('validate_login/', ValidateLoggedInAPIView.as_view()),
 ]
--- varta/accounts/views.py
+++ varta/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
 
 class LogoutAPIView(GenericAPIView):
     queryset = Token.objects.all()
@@ -15,3 +17,12 @@ class LogoutAPIView(GenericAPIView):
         return Response(
             {"detail": "Logout successful"},
             status=status.HTTP_200_OK)
+
+class ValidateLoggedInAPIView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        user_serializer = UserSerializer(request.user)
+        return Response( user_serializer.data, status=status.HTTP_200_OK )
